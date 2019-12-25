e4ac1b935856a153d193027d4d22819136c7260c
Oksana <prokopekoksana@gmail.com>
2019-12-25 20:12:27 +0100

    Update test1_login.py

--- test1_login.py
+++ test1_login.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.chrome.webdriver import WebDriver
 import unittest
+from fields import Fields
 
 def is_alert_present(wd):
     try:
@@ -17,13 +18,13 @@ class login(unittest.TestCase):
         wd.get("file:///C:/Users/proko/Documents/GitHub/Zadanie_Piwik/index2%20(2).html")
 
 
-    def populating_fields(self, wd, firstname, lastname):
+    def populating_fields(self, wd, fields):
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(firstname)
+        wd.find_element_by_name("firstname").send_keys(fields.firstname)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(lastname)
+        wd.find_element_by_name("lastname").send_keys(fields.lastname)
 
 
     def submit_data(self, wd):
@@ -33,28 +34,35 @@ class login(unittest.TestCase):
     def test_login(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.populating_fields(wd, firstname="Oksana", lastname="Prokopek")
+        self.populating_fields(wd, Fields(firstname="Oksana", lastname="Prokopek"))
         self.submit_data(wd)
 
 
     def test_empty_login(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.populating_fields(wd, firstname="   ", lastname="   ")
+        self.populating_fields(wd, Fields(firstname="", lastname=""))
+        self.submit_data(wd)
+
+
+    def test_with_enter(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.populating_fields(wd, Fields(firstname="   ", lastname="   "))
         self.submit_data(wd)
 
 
     def test_spec_char(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.populating_fields(wd, firstname="d426373!@#$%^&*()_+}{:?><", lastname="d426373!@#$%^&*()_+}{:?><")
+        self.populating_fields(wd, Fields(firstname="d426373!@#$%^&*()_+}{:?><", lastname="d426373!@#$%^&*()_+}{:?><"))
         self.submit_data(wd)
 
 
     def test_max_length(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.populating_fields(wd, firstname="Start khgkudhfuhgkdfhog sldnvgldjnrgjndjlfngbjdnlbjngfnkjngfbkjnkjg73ryehwifhrkghdf"
+        self.populating_fields(wd, Fields(firstname="Start khgkudhfuhgkdfhog sldnvgldjnrgjndjlfngbjdnlbjngfnkjngfbkjnkjg73ryehwifhrkghdf"
                                              "sfhvbksbfkuksvbnfkskbfndkndbknkdunfknkdbjfkdurotiguw48t938w4u98tugef948ug9e8r9hu8rth"
                                              "dufhbogudofbuijdotibjoeitjboinjtrnojhtboiejr8ot3 oiwrjgo3ij4oe59hjr0e59jh950hjr9t50h riej"
                                              "eirojgboeirjboijetoibjojgtrnoibjrgoijrjntoietoigjot83u4r5o38409 iwvngoierngoienrooijgierjg"
@@ -63,14 +71,14 @@ class login(unittest.TestCase):
                                         "dfkbmlkngflbjkgfnnlkfgklmflkgmnlkfmgnmkllfkmgobn ksjdngvkjsnkdvbf fjnvglbdjfnbljnclgbkn"
                                         "djfnbjdngbljndlgknblkdnglkbmldkmnlkndflkbnlgjnlbngfbkdjgkbjkf dlkfbhjldjitglhdjgfblknldkg"
                                         "df;kmblkdgmblkmdglknmlfmdfhbndljtnguheotoy8e5roigj jrglidjflhigdlj ldjgldjigjpdifjgnl djvnfds"
-                                        "lsnfvblksnlfsm fdsjbnlsijrojifwoeurjoqw83u4982398ru49i7ytei547yt985ue9y804e58tguoerghero")
+                                        "lsnfvblksnlfsm fdsjbnlsijrojifwoeurjoqw83u4982398ru49i7ytei547yt985ue9y804e58tguoerghero"))
         self.submit_data(wd)
 
 
     def test_enter_link(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.populating_fields(wd, firstname="http://www.montypython.com/", lastname="http://www.montypython.com/")
+        self.populating_fields(wd, Fields(firstname="http://www.montypython.com/", lastname="http://www.montypython.com/"))
         self.submit_data(wd)
 
 
